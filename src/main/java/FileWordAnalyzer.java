fc727a2a4fc7ca6b0017c5025c8b53f3eda0b5f1
Ozukyo <ozukyo@gmail.com>
2020-07-16 20:40:55 +0200

    Add FileWordAnalyzer class

--- src/main/java/FileWordAnalyzer.java
+++ src/main/java/FileWordAnalyzer.java
@@ -1,7 +1,63 @@
+import java.io.IOException;
+import java.util.*;
+
 public class FileWordAnalyzer {
     private FilePartReader fpr;
 
     public FileWordAnalyzer(FilePartReader fpr) {
         this.fpr = fpr;
     }
+
+    public List<String> getWordsOrderedAlphabetically() throws IOException {
+        String[] arrayOfWords = getArrayOfWords();
+
+        List<String> list = Arrays.asList(arrayOfWords);
+        Collections.sort(list);
+
+        return list;
+    }
+
+    public List<String> getWordsContainingSubstring(String subString) throws IOException {
+        String[] arrayOfWords = getArrayOfWords();
+        List<String> listOfSubstrings = new ArrayList<>();
+
+        for (String s : arrayOfWords) {
+            if (s.contains(subString)) {
+                listOfSubstrings.add(s);
+            }
+        }
+        return listOfSubstrings;
+    }
+
+    public List<String> getStringsWhichPalindromes() throws IOException {
+        String[] arrayOfWords = getArrayOfWords();
+        List<String> listOfPalindromes = new ArrayList<>();
+
+        for(String s: arrayOfWords) {
+            char[] wordInChars= s.toCharArray();
+            int firstIndex = 0;
+            int lastIndex = wordInChars.length-1;
+
+            while(lastIndex > firstIndex) {
+                if(wordInChars[firstIndex] != wordInChars[lastIndex]) {
+                    break;
+                }
+                ++firstIndex;
+                --lastIndex;
+            }
+
+            if(lastIndex!=wordInChars.length-1) {
+                listOfPalindromes.add(s);
+            }
+
+        }
+        return listOfPalindromes;
+
+    }
+
+    public String[] getArrayOfWords() throws IOException {
+        String str = fpr.readLines().replaceAll("[^a-zA-Z]+", " ");
+        return str.toLowerCase().split(" ");
+    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,16 @@
+
 import java.io.IOException;
+import java.net.URL;
+
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
        FilePartReader filePartReader = new FilePartReader();
-       filePartReader.setup("/home/ozukyo/Dokumenty/web/assignments/filepartreader-testing-with-junit-Ozukyo/src/resources/sample.txt", 1, 1);
-//        System.out.println(filePartReader.read());
-        System.out.println(filePartReader.readLines());
+       URL url = Main.class.getResource("sample.txt");
+       filePartReader.setup((url.getPath()), 1, 1);
+       FileWordAnalyzer fileWordAnalyzer = new FileWordAnalyzer(filePartReader);
+       System.out.println(fileWordAnalyzer.getStringsWhichPalindromes());
+
     }
 }
